6ccda14588d10de01ee4e12238ce5901c7514619
rproskuryakov <noisyrave@gmail.com>
2025-08-09 16:32:37 +0400

    Import cdktf and google/digitalocean/aws providers in infra package

--- src/infra.go
+++ src/infra.go
@@ -1,3 +1,9 @@
 package infra
 
 
+import (
+    "github.com/hashicorp/terraform-cdk-go/cdktf"
+    "github.com/hashicorp/cdktf-provider-google-go/google"
+    "github.com/hashicorp/cdktf-provider-digitalocean-go/digitalocean"
+    "github.com/hashicorp/cdktf-provider-aws-go/aws"
+)
